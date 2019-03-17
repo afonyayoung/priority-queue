5791a930612f90673d94588547509e8525fefdf7
afonyayoung <xafonya@gmail.com>
2019-03-17 21:07:59 +0300

    max-heap and queue basis

--- src/max-heap.js
+++ src/max-heap.js
@@ -2,15 +2,20 @@ const Node = require('./node');
 
 class MaxHeap {
 	constructor() {
-		
+		this.root = null;
+		this.parentNodes = [];
 	}
 
 	push(data, priority) {
-		
+		let nodePush = new Node(data,priority);
+		this.insertNode(nodePush);
+		this.shiftNodeUp(nodePush);	
 	}
 
 	pop() {
-		
+		if(!this.root){
+			return;
+		}
 	}
 
 	detachRoot() {
@@ -22,7 +27,6 @@ class MaxHeap {
 	}
 
 	size() {
-		
 	}
 
 	isEmpty() {
@@ -30,19 +34,52 @@ class MaxHeap {
 	}
 
 	clear() {
+		this.root = null;
+		this.parentNodes=[];
 		
 	}
 
 	insertNode(node) {
-		
+		if (this.root == null) {
+			this.root = node;
+			this.parentNodes.push(node);
+		} else {
+			this.parentNodes[0].appendChild(node);
+			if (this.parentNodes[0].left != null && this.parentNodes[0].right != null) {
+				this.parentNodes.shift();
+			}
+			this.parentNodes.push(node);
+		}
+	}
+
+	correctParentNodes(node){
+		let pos = this.parentNodes.indexOf(node);
+		let parentPos = this.parentNodes.indexOf(node.parent);
+
+		if (parentPos !== -1) {
+			this.parentNodes[pos] = node.parent;
+			this.parentNodes[parentPos] = node;
+		}
+		else {
+			this.parentNodes[pos] = node.parent;
+		}
+
 	}
 
 	shiftNodeUp(node) {
-		
+		if (node.parent == null){
+			this.root = null;
+		} else {
+			if (node.parent.priority < node.priority){
+				this.correctParentNodes(node);
+				node.swapWithParent();
+				this.shiftNodeUp(node);
+			} 
+			this.root = node;
+		}
 	}
 
 	shiftNodeDown(node) {
-		
 	}
 }
 
--- src/queue.js
+++ src/queue.js
@@ -2,11 +2,20 @@ const MaxHeap = require('./max-heap.js');
 
 class PriorityQueue {
 	constructor(maxSize) {
-
+		if(maxSize){
+			this.maxSize = maxSize
+		} else {
+			this.maxSize = 30;
+		}
+		this.heap = new MaxHeap;
 	}
 
 	push(data, priority) {
-
+		if(this.heap.size() >= this.maxSize){
+			throw new SyntaxError("error");
+		}else{
+			this.heap.push(data,priority);
+		}
 	}
 
 	shift() {
@@ -14,7 +23,7 @@ class PriorityQueue {
 	}
 
 	size() {
-
+		
 	}
 
 	isEmpty() {
